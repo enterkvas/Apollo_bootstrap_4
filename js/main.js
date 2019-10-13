337e36af8497d46f64530ae4de76246faf311f6b
enterkvas <enterkvas@gmail.com>
2019-10-13 22:18:40 +0300

    Tenth commit - connected slider

--- js/main.js
+++ js/main.js
@@ -63,13 +63,13 @@ $(document).ready(function() {
 	};
 
 	// Вызов слайдера owl-carousel
-	// $("#top-slider").owlCarousel({
-	// 	singleItem: true,
-	// 	navigation: true,
-	// 	theme: "top-slider-theme",
-	// 	navigationText : ["",""],
-	// 	slideSpeed: 600
-	// });
+	$("#top-slider").owlCarousel({
+		singleItem: true,
+		navigation: true,
+		// theme: "top-slider-theme",
+		// navigationText : ["",""],
+		// slideSpeed: 600
+	});
 
 	//slide2id - плавная прокрутка по ссылкам внутри страницы
 	// $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
